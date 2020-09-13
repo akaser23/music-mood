296010d70476f5c7c2e0850985e1f87e2b8c858a
Jordan Roberts <jdrobs19@gmail.com>
2020-09-13 12:08:37 -0600

    have fetch for spotify

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,32 +98,29 @@ var displayPlaylist = function (weather) {
     console.log(weather.weather[0].main);
 
     if (weather.weather[0].main === "Clouds") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/0gkNFS5NKEO32illt0eyeF" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/0gkNFS5NKEO32illt0eyeF" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].main === "Thunderstorm") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/0bQLlEp1rwCO06qzmuBsBN" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/0bQLlEp1rwCO06qzmuBsBN" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].main === "Drizzle") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/0tjLWaFVAVegrqTXyqrhSD" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/0tjLWaFVAVegrqTXyqrhSD" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].main === "Rain") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/3LndYn1dDYsphyFzTXbc0g" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/3LndYn1dDYsphyFzTXbc0g" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].main === "Snow") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/4PH4kPc5WhaKH9kuuoJaqw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/4PH4kPc5WhaKH9kuuoJaqw" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].icon === "50d") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/4115GWyiQncaTuQmtDNefr" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/4115GWyiQncaTuQmtDNefr" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
     else if (weather.weather[0].main === "Clear") {
-        playlistEl.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1BzILRveYHb" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+        playlistEl.innerHTML = '<iframe id="myFrame" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX1BzILRveYHb" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
 };
 
 cityFormEl.addEventListener("submit",formSubmitHandler);
-
-
-//var artist = document.querySelector("#main > div > div > div.ai.au.ah.av > div.ah.bk.bl > div > div.ah.bm.b6.bn > a > div:nth-child(2) > span").textContent;
 
 var token = function(){
     fetch("https://accounts.spotify.com/api/token", {
